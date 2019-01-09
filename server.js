f5a2cc2e1f3f92c1c23710174850bb87157c662d
Aman <amsingh714@gmail.com>
2019-01-09 18:53:26 -0500

    formmated code

--- server.js
+++ server.js
@@ -1,74 +1,71 @@
 const express = require("express");
 const db = require("./data/helpers/userDb");
 
-
 const server = express();
-
-
-
-
 
 // MIDDLEWARE
 server.use(express.json());
 
 const uppercase = (req, res, next) => {
-    req.body.name = req.body.name.toUpperCase();
-    next();
-}
-
-
+  req.body.name = req.body.name.toUpperCase();
+  next();
+};
 
 // Display Hello to homepage.
 server.get("/", (req, res) => {
-    res.send("Hello.")
-})
-
+  res.send("Hello.");
+});
 
 // GET - READ
 server.get("/users", (req, res) => {
-    db.get()
+  db.get()
     .then(users => {
-        res.status(200).json(users);
+      res.status(200).json(users);
     })
     .catch(err => {
-        res.status(500).json({ error: "The users could not be retrieved." })
-    })
-})
-
+      res.status(500).json({ error: "The users could not be retrieved." });
+    });
+});
 
 // GET - READ with specific ID
 server.get("/users/:userId", (req, res) => {
-    const id = req.params.userId
+  const id = req.params.userId;
 
-    db.get(id)
+  db.get(id)
     .then(user => {
-        res.status(200).json(user);
+      res.status(200).json(user);
     })
     .catch(err => {
-        res.status(500).json({ error: "The user with the specific ID does not exist." })
-    })
-})
+      res
+        .status(500)
+        .json({ error: "The user with the specific ID does not exist." });
+    });
+});
 
+// POST - CREATE
+server.post("/users", uppercase, (req, res) => {
+  const user = req.body;
 
-// POST - CREATE 
-server.post("/users", uppercase, (req,res) => {
-    const user = req.body;
-
-    if (!user.name) {
-        res.status(400).json({ error: "Please provide a name for the user." })
-    } else if (user.name.length > 128) {
-        res.status(400).json({ error: "Please provide a name that is under 129 characters." })
-    } else {
-        db.insert(user)
-        .then(result => {
-            res.status(201).json(result);
-        })
-        .catch(err => {
-            res.status(500).json({ error: "There was an error while saving the user to the database" })
-        })
-    }
-})
-
+  if (!user.name) {
+    res.status(400).json({ error: "Please provide a name for the user." });
+  } else if (user.name.length > 128) {
+    res
+      .status(400)
+      .json({ error: "Please provide a name that is under 129 characters." });
+  } else {
+    db.insert(user)
+      .then(result => {
+        res.status(201).json(result);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({
+            error: "There was an error while saving the user to the database"
+          });
+      });
+  }
+});
 
 // DELETE
 server.delete("/users/:userId", (req, res) => {
@@ -91,29 +88,30 @@ server.delete("/users/:userId", (req, res) => {
     });
 });
 
-
 // PUT - UPDATE
 server.put("/users/:userId", uppercase, (req, res) => {
-    const id = req.params.userId;
-    const changes = req.body;
+  const id = req.params.userId;
+  const changes = req.body;
 
-    db.get(id)
+  db.get(id)
     .then(user => {
-        if (!user) {
-            res.status(404).json({ message: "The user with the specified ID does not exist." })
-        }
-        if (!changes.name) {
-            res.status(400).json({ error: "Please provide a name for the user."})
-        }
-        db.update(id, changes).then(result => {
-            res.status(200).json({result})
-        })
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
+      if (!changes.name) {
+        res.status(400).json({ error: "Please provide a name for the user." });
+      }
+      db.update(id, changes).then(result => {
+        res.status(200).json({ result });
+      });
     })
     .catch(err => {
-        res.status(500).json({ error: "The user information could not be modified." })
-    })
-})
-
-
+      res
+        .status(500)
+        .json({ error: "The user information could not be modified." });
+    });
+});
 
 module.exports = server;
