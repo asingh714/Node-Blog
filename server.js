1d1548830c8885e5988d2e375c972ae26563b3fb
Aman <amsingh714@gmail.com>
2019-01-09 17:25:46 -0500

    able to UPDATE user

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ server.post("/users", (req,res) => {
     const user = req.body;
 
     if (!user.name) {
-        res.status(400).json({ error: "Please provide a name for the user" })
+        res.status(400).json({ error: "Please provide a name for the user." })
     } else {
         db.insert(user)
         .then(result => {
@@ -75,6 +75,28 @@ server.delete("/users/:userId", (req, res) => {
     });
 });
 
+
+// PUT - UPDATE
+server.put("/users/:userId", (req, res) => {
+    const id = req.params.userId;
+    const changes = req.body;
+
+    db.get(id)
+    .then(user => {
+        if (!user) {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+        if (!changes.name) {
+            res.status(400).json({ error: "Please provide a name for the user."})
+        }
+        db.update(id, changes).then(result => {
+            res.status(200).json({result})
+        })
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The user information could not be modified." })
+    })
+})
 
 
 
