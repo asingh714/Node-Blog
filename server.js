f5e26cefc995700794eade20f46c5cf61d65a7f3
Aman <amsingh714@gmail.com>
2019-01-09 17:02:33 -0500

    able to POST user

--- server.js
+++ server.js
@@ -36,5 +36,23 @@ server.get("/users/:userId", (req, res) => {
 })
 
 
+// POST - CREATE 
+server.post("/users", (req,res) => {
+    const user = req.body;
+
+    if (!user.name) {
+        res.status(400).json({ error: "Please provide a name for the user" })
+    } else {
+        db.insert(user)
+        .then(result => {
+            res.status(201).json(result);
+        })
+        .catch(err => {
+            res.status(500).json({ error: "There was an error while saving the user to the database" })
+        })
+    }
+
+})
+
 
 module.exports = server;
