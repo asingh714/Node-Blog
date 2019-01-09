f3b23f9009cbf119fc8b6c36489f55f9cc022e4a
Aman <amsingh714@gmail.com>
2019-01-09 16:50:01 -0500

    able to READ all users

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const db = require("./data/helpers/userDb");
+
 
 const server = express();
+server.use(express.json());
 
-module.exports = server;
 
 server.get("/", (req, res) => {
     res.send("Hello.")
 })
+
+
+server.get("/users", (req, res) => {
+    db.get()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The users could not be retrieved." })
+    })
+})
+
+
+
+module.exports = server;
