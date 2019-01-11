ddf43e9d98f58ebb049e6f3960edf53597730b0a
Aman <amsingh714@gmail.com>
2019-01-11 15:43:25 -0500

    able to READ post for specific ID

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -1,20 +1,31 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-const db = require("../data/helpers/postDb")
+const db = require("../data/helpers/postDb");
 
-
-// GET - READ 
+// GET - READ
 router.get("/", (req, res) => {
-    db.get()
+  db.get()
     .then(posts => {
-        res.status(200).json(posts)
+      res.status(200).json(posts);
     })
     .catch(err => {
-        res.status(500).json({ error: "The posts could not be retrieved." });
+      res.status(500).json({ error: "The posts could not be retrieved." });
+    });
+});
+
+// GET - READ with specific ID 
+router.get("/:postId", (req, res) => {
+  const id = req.params.postId;
+  
+  db.get(id)
+    .then(post => {
+      res.status(200).json(post);
     })
-})
-
-
-
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The post with the specific id does not exist." });
+    });
+});
 
 module.exports = router;
