a2c53ee3a022f4a3651ca3b232ab5e74a92354f7
Aman <amsingh714@gmail.com>
2019-03-20 17:32:11 -0400

    able to GET post with specific ID

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -17,4 +17,24 @@ router.get("/", (req, res) => {
     });
 });
 
+// GET POSTS w/ Specific ID
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db.getById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "The post with the specified ID does not exist."
+      });
+    });
+});
+
 module.exports = router;
