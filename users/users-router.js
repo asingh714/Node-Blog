1c9bb3748ddef9c517c878c484111573064a5747
Aman <amsingh714@gmail.com>
2019-03-20 17:09:11 -0400

    able to GET lists of posts by user

--- users/users-router.js
+++ users/users-router.js
@@ -17,4 +17,51 @@ router.get("/", (req, res) => {
     });
 });
 
+// GET USERS BY ID
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.getById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The user with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The user with the specified ID does not exist." });
+    });
+});
+
+// GET LISTS OF POSTS BY USER
+router.get("/posts/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.getUserPosts(id)
+    .then(posts => {
+      if (posts.length > 0) {
+        res.status(200).json(posts);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The posts for the specified ID do not exist" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error:
+          "The list of posts by the user with the specified ID does not exist."
+      });
+    });
+});
+
+
+
+
+
 module.exports = router;
