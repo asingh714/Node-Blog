df409b39018df022588699f7670594d400dae25d
Aman <amsingh714@gmail.com>
2019-03-20 16:53:46 -0400

    added users-routers file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require("express"); 
+const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
+
+const usersRouter = require("./users/users-router.js");
 
 const server = express();
 
@@ -8,5 +10,7 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 server.use(morgan("short"));
+
+server.use("/api/users", usersRouter);
 
 module.exports = server;
---
+++ users/users-router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const db = require("../data/helpers/userDb");
+
+const router = express.Router();
+
+
+
+module.exports = router;
