368b98cd767fc995bfd061ea30500495bfacd7d5
Aman <amsingh714@gmail.com>
2019-03-20 16:54:22 -0400

    able to GET users

--- users/users-router.js
+++ users/users-router.js
@@ -4,6 +4,17 @@ const db = require("../data/helpers/userDb");
 
 const router = express.Router();
 
-
+// GET USERS
+router.get("/", (req, res) => {
+  db.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved. " });
+    });
+});
 
 module.exports = router;
